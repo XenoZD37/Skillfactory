fc4c60825b6089e81ac8b37a181a6e23908d1e98
Мамыкин Даниил <60557109+XenoZD37@users.noreply.github.com>
2022-06-06 12:52:49 +0300

    13.8.19 solution

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-per_cent = {'ТКБ': 5.6, 'СКБ': 5.9, 'ВТБ': 4.28, 'СБЕР': 4.0}
 if __name__ == '__main__':
-    money = int(input())
-    deposit = list(i * money / 100 for i in per_cent.values())
-    print(deposit)
-    print("Максимальная сумма, которую вы можете заработать —", max(deposit))
+    ticket_quantity = int(input("Number of tickets: "))
+    price_sum = 0
+    for i in range(ticket_quantity):
+        price_current = int(input("Age of ticket " + str(i + 1) + " owner: "))
+        if price_current >= 0 and price_current < 18:
+            pass
+        elif (price_current >= 18 and price_current < 25):
+            price_sum += 990
+        elif (price_current >= 25):
+            price_sum += 1390
+    if (ticket_quantity > 3):
+        price_sum *= 0.9
+    print(price_sum)
